47c51907753f1481ce54935d368cb79f2dabb8ba
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-08 16:31:00 +0100

    Parser: Getting object from url

---
+++ Models/API_Parser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StivePC.Models
+{
+	internal class API_Parser
+	{
+		public static Object? GetClass( string url )
+		{
+			string pattern = @"\/([A-Z])\w+";
+			Regex regex = new( pattern );
+			MatchCollection matchCollection = regex.Matches( url );
+			string className = matchCollection[ 0 ].Value[ 1.. ];
+			Type? t = Type.GetType( "StivePC.Models." + className );
+
+			return t != null ? Activator.CreateInstance( t ) : null;
+		}
+	}
+}
