1f7163038c814abc117f8877b500372149d4c9c0
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-11 20:15:07 +0100

    Added converter (JSON to object) Single JSON object accepted

---
+++ Models/Converter.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StivePC.Models
+{
+	internal class Converter
+	{
+		public static dynamic? JSONToObject( JObject data, Type objectType )
+		{
+			Type objType = GetFullTypeName( objectType );
+			dynamic? obj = Activator.CreateInstance( objType );
+
+			if ( obj == null )
+			{
+				return null;
+			}
+
+			PropertyInfo[] propertyInfos = objType.GetProperties();
+
+			foreach ( PropertyInfo propertyInfo in propertyInfos )
+			{
+				string propertyName  = propertyInfo.Name;
+				var	 propertyValue = data.GetValue( propertyName );
+				Type   propertyType	= propertyInfo.PropertyType;
+
+				var value = Convert.ChangeType( propertyValue, propertyType );
+				propertyInfo.SetValue( obj, value );
+			}
+
+			return obj;
+		}
+
+		public static Type GetFullTypeName( Type type )
+		{
+			string assemblyName = type.AssemblyQualifiedName ?? type.Name;
+			Type?  fullTypeName = Type.GetType( assemblyName );
+
+			return fullTypeName ?? type;
+		}
+	}
+}
