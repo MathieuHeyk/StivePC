3a24fabd35034d3f1a8bbd1ec5eff34755034a0d
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-12 11:55:35 +0100

    Getting all elements function added

--- Models/DatabaseTable.cs
+++ Models/DatabaseTable.cs
@@ -10,23 +10,64 @@ namespace StivePC.Models
 {
 	internal class DatabaseTable
 	{
-		public dynamic? GetElementsById( int id, string url = "https://localhost:7201/" )
+		public dynamic? GetElementById( int id )
 		{
-			API api = API.GetInstance( url );
 			string tableName	= GetTableName( this );
 		//	string idName		= GetIdName( this ); // "idName" is always "id"
-			string parameters = tableName + "/" + "Get" + tableName + "ById?id=" + id;
-			JObject? result	= api.Query( parameters );
+			string parameters = tableName + "/Get" + tableName + "ById?id=" + id;
+			JObject? result	= API.Query( parameters );
 
 			if ( result == null )
 			{
 				return null;
 			}
 
-			return Converter.JSONToObject( result, this.GetType() );
+			return Converter.JSONToObject( result, GetType() );
 		}
 
-		public string GetTableName( object table )
+		/** 
+		 *	<remarks>Need casting result into the type.
+		 *	<example>For example:
+		 *	<code>
+		 *	Article art = new Article();
+		 *	object[]? listsOfArticles = art.GetAllElements();
+		 *	</code>
+		 *	</example>
+		 *	</remarks>
+		 *	<returns></returns>
+		 */
+		public dynamic[]? GetAllElements()
+		{
+		// ToDo: Move this block inside API object ?
+			string tableName  = GetTableName( this );
+			string parameters = tableName + "/GetAll" + tableName + "x";
+			JArray? result 	= API.Query( parameters );
+
+			if ( result == null )
+			{
+				return null;
+			}
+
+			int totalElements  = result.Count;
+			dynamic[] elements = new dynamic[ totalElements ];
+
+			for ( int i = 0; i < totalElements; i++ )
+			{
+				JObject jsonElement = ( JObject ) result[ i ];
+				var element = Converter.JSONToObject( jsonElement, GetType() );
+
+				if ( element == null )
+				{
+					return null;
+				}
+
+				elements[ i ] = element;
+			}
+
+			return elements;
+		}
+
+		public static string GetTableName( object table )
 		{
 			return table.GetType().Name;
 		}
