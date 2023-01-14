9428e5cf445c48d6d0939fce3972aa11fd220abb
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-14 22:36:29 +0100

    Added doc for database class Summary, param and small explanation

--- Models/Database.cs
+++ Models/Database.cs
@@ -6,14 +6,19 @@ namespace StivePC.Models
 {
 	internal class Database
 	{
-		public static void Add( Object elementToAdd )
+	/**
+	 * <summary>Add an element to the database (using the API)</summary>
+	 * <param name="element">The element to add</param>
+	 */
+		public static void Add( Object element )
 		{
-			Type objectType			 = elementToAdd.GetType();
+			Type objectType			 = element.GetType();
 			List<dynamic> parameters = new();
 
+		// Loop starts with the second property (no need of ID)
 			foreach ( PropertyInfo property in objectType.GetProperties()[ 1.. ] )
 			{
-				dynamic? propertyValue = property.GetValue( elementToAdd );
+				dynamic? propertyValue = property.GetValue( element );
 				string	valueAsString = Converter.ForURL( propertyValue );
 				parameters.Add( property.Name + "=" + valueAsString );
 			}
