0a6367b85251cfec1514ef48c577c40ee7051083
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-14 22:27:01 +0100

    Added reworked database actions file Function to add an element to the database (using the API)

---
+++ Models/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace StivePC.Models
+{
+	internal class Database
+	{
+		public static void Add( Object elementToAdd )
+		{
+			Type objectType			 = elementToAdd.GetType();
+			List<dynamic> parameters = new();
+
+			foreach ( PropertyInfo property in objectType.GetProperties()[ 1.. ] )
+			{
+				dynamic? propertyValue = property.GetValue( elementToAdd );
+				string	valueAsString = Converter.ForURL( propertyValue );
+				parameters.Add( property.Name + "=" + valueAsString );
+			}
+
+			string url = String.Format( "{0}/Add{0}?", objectType.Name )
+						  + String.Join( '&', parameters );
+
+			API.PostQuery( url );
+		}
+	}
+}
