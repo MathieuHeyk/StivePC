ba3bbdfa1954308d05ef3ce302c0da671bc78f0b
Mathieu Heyk <mathieu.heyk@outlook.fr>
2023-01-18 15:07:59 +0100

    Added 'Session' object Saving current user data into Session object (Singleton)

---
+++ Models/Session.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StivePC.Models
+{
+	internal class Session
+	{
+		private static Session? _instance;
+		private Utilisateur _currentUser;
+
+		private Session( Utilisateur user )
+		{
+			_currentUser = user;
+		}
+
+		public static Session GetInstance( Utilisateur? user = null )
+		{
+			_instance ??= new Session( user ?? new Utilisateur() );
+			return _instance;
+		}
+
+		public Utilisateur GetUser()
+		{
+			return _currentUser;
+		}
+	}
+}
